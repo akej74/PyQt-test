8b69d9ea8214ba0e69959f42caa5d6c68b0b5ba3
p3t3rm <p3t3rm@email.com>
2016-06-09 22:47:30 +0200

    Simplified window class

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 from design import Ui_MainWindow
 
-class MyWindow(Ui_MainWindow):
+
+class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        ui = Ui_MainWindow()
+        ui.setupUi(self)
 
 
 if __name__ == "__main__":
-
-    # Create the main QT application
     app = QtWidgets.QApplication(sys.argv)
 
-    # Create the main window
-    MainWindow = QtWidgets.QMainWindow()
+    win = MyWindow()
+    win.show()
 
-    ui = MyWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-
-    # Exit QT application
     sys.exit(app.exec_())
-
