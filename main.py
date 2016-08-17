e8b9568637ad971809f8f9d4d7857eada711ea00
Ake Johansson <ake.home@gmail.com>
2016-08-17 16:11:37 +0200

    Test with threading...

--- main.py
+++ main.py
@@ -2,19 +2,31 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from DiceSimulatorUI import Ui_MainWindow
 from random import randint
+import time
 
-#class DiceThread(QtCore.QThread):
-#    def __init__(self):
-#        super.__init__()
-#
-#    def __del__(self):
-#        self.wait()
-#
-#    def run(self):
-#        pass
+class DiceThread(QtCore.QThread):
+    #dice_throw_signal = QtCore.pyqtSignal(int)
+
+    def __init__(self, dices, throws_per_second):
+        super().__init__()
+        self.dices = dices
+        self.throws_per_second = throws_per_second
+
+    def __del__(self):
+       self.wait()
+
+
+
+    def run(self):
+        print("Run method!")
+        print(self.dices)
+        print(self.throws_per_second)
+
+
 
 
 class DiceSimulator(QtWidgets.QMainWindow):
+
     dice_throw_signal = QtCore.pyqtSignal(int)
 
     def __init__(self):
@@ -31,20 +43,30 @@ class DiceSimulator(QtWidgets.QMainWindow):
         # Connect the "button start click" to the "button_start_clicked" method
         self.ui.buttonStart.clicked.connect(self.button_start_clicked)
 
+        self.thread = DiceThread(self.ui.spinBoxDices.value(), self.ui.spinBoxThrows.value())
+
+
+
     def button_start_clicked(self):
         # Send the value of the spin box "Number of dices" to the "dicethrow" method
-        self.dicethrow(self.ui.spinBoxDices.value())
+        #self.dicethrow(self.ui.spinBoxDices.value())
+        self.thread.start()
+
+
 
     def dicethrow(self, dices):
 
-        dice_sum = 0
+        #for t in range(5):
+            dice_sum = 0
+            for d in range(dices):
+                current_dice = self.dice()
+                dice_sum += current_dice
 
-        for d in range(dices):
-            current_dice = self.dice()
-            dice_sum += current_dice
+            #time.sleep(0.5)
 
-        # Emit the "dice_throw" signal
-        self.dice_throw_signal.emit(dice_sum)
+            # Emit the "dice_throw" signal
+            self.dice_throw_signal.emit(dice_sum)
+            print("Dice throw " + str(t))
 
 
     def dice(self):
